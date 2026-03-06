8227a7656a53db07f70a0420872ce9e66637e925
Isaac de Medeiros <linzackmedeiros@gmail.com>
2026-03-06 19:11:35 -0300

    feat error handler para erros

--- src/middleware/errorHandler.ts
+++ src/middleware/errorHandler.ts
@@ -2,3 +2,24 @@ import { Request, Response, NextFunction } from "express";
 import { AppError } from "../model/appError"
 import { StatusCode } from "../util/utilNumbers";
 
+export function errorHandler(
+    err: Error,
+    req: Request,
+    res: Response,
+    next: NextFunction
+) {
+    const isAppError = err instanceof AppError;
+    const statusCode = isAppError ? err.statusCode: StatusCode.SERVER_ERROR;
+    const message = isAppError
+        ? err.message
+        : "Erro interno do servidor";
+    console.error({
+        name: err.name,
+        message: err.message,
+        stack: err.stack
+    });
+
+    return res.status(statusCode).json({
+        error: message
+    })
+}
