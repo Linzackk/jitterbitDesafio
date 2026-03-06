7f9decbc03eb7c141a03d91c781921fa2836803a
Isaac de Medeiros <linzackmedeiros@gmail.com>
2026-03-06 19:15:11 -0300

    feat uso do Error Handler no apP

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,13 @@
 import express from "express";
 import orderService from "./routes/order.routes";
+import { errorHandler } from "./middleware/errorHandler";
 
 const app = express();
 
 app.use(express.json());
 
 app.use("/order", orderService);
+
+app.use(errorHandler);
 
 export default app;
